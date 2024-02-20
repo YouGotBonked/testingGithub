cf0d32c88219c0c89f8c8f2c52d7c032065f4a6d
YouGotBonked <113187422+YouGotBonked@users.noreply.github.com>
2024-02-20 12:44:23 -0500

    Revert "for loop"
    This reverts commit ed0bd0b11c02a83899ae984a42e8279392628271.

--- FirstGithub/src/Matthew_Hiro.java
+++ FirstGithub/src/Matthew_Hiro.java
@@ -4,10 +4,6 @@ public class Matthew_Hiro {
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
         System.out.println("Hello Hiro");
-        for(int i= 0; i< 16; i++)
-        {
-            System.out.println("I am " + i + " years old :D");
-        }
-
+        
     }
 }
