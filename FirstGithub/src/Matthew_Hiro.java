ed0bd0b11c02a83899ae984a42e8279392628271
RachelLu2008 <113115804+RachelLu2008@users.noreply.github.com>
2024-02-20 12:21:15 -0500

    for loop

--- FirstGithub/src/Matthew_Hiro.java
+++ FirstGithub/src/Matthew_Hiro.java
@@ -4,6 +4,10 @@ public class Matthew_Hiro {
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
         System.out.println("Hello Hiro");
-        
+        for(int i= 0; i< 16; i++)
+        {
+            System.out.println("I am " + i + " years old :D");
+        }
+
     }
 }
