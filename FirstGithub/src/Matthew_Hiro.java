d3d1d5e146b2293815164cc083b551f193ddadcd
RachelLu2008 <113115804+RachelLu2008@users.noreply.github.com>
2024-02-20 12:21:05 -0500

    hello

--- FirstGithub/src/Matthew_Hiro.java
+++ FirstGithub/src/Matthew_Hiro.java
@@ -1,2 +1,9 @@
 public class Matthew_Hiro {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+        System.out.println("Hello Hiro");
+        
+    }
 }
